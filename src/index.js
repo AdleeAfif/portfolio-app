30b97466f4d31ff74a3b911bb1cb3d1f9c30dc6d
AdleeAfif <kamitokazuya@gmail.com>
2023-09-06 14:40:20 +0800

    Change background color

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./view/App";
+import reportWebVitals from "./reportWebVitals";
+import Header from "./view/component/header";
+import Footer from "./view/component/footer";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path="/" component={App} />
+        <Route path="/users" component={App} />
+        <Route path="/contact" component={App} />
+        <Route component={App} />
+      </Routes>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
