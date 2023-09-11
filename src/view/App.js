18d2c7cb8cda642fc1093cc045418ad6aec78191
AdleeAfif <kamitokazuya@gmail.com>
2023-09-11 13:38:02 +0800

    Added Header and Routes

--- src/view/App.js
+++ src/view/App.js
@@ -1,10 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
 import "./App.css";
+import Header from "./component/header";
+import About from "./about/about";
 import Home from "./home/home";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
